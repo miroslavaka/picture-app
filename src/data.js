ea19ea44a712464fc49a7a7f0d483db432e18d97
miroslavaka <kacmarova.miroslava@gmail.com>
2023-03-12 19:29:31 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,132 @@
+const images = [
+  {
+    id: 1,
+    img: '../../images/apples-1.jpg',
+    category: ['nature', 'fruits', 'apples', 'red', 'green'],
+  },
+  {
+    id: 2,
+    img: '../../images/apples-2.jpg',
+    category: ['nature', 'fruits', 'apples', 'pink', 'green'],
+  },
+  {
+    id: 3,
+    img: '../../images/beetles.jpg',
+    category: ['nature', 'beetles', 'insect', 'pink', 'green'],
+  },
+  {
+    id: 4,
+    img: '../../images/bird-1.jpg',
+    category: ['nature', 'bird', 'animal', 'yellow'],
+  },
+  {
+    id: 5,
+    img: '../../images/bird-2.jpg',
+    category: ['nature', 'bird', 'animal', 'yellow', 'pink'],
+  },
+  {
+    id: 6,
+    img: '../../images/butterfly.jpg',
+    category: ['nature', 'butterfly', 'animal', 'insect', 'red', 'green'],
+  },
+  {
+    id: 7,
+    img: '../../images/cherry-1.jpg',
+    category: ['nature', 'flower', 'blossom', 'white', 'blue', 'insect'],
+  },
+  {
+    id: 8,
+    img: '../../images/cherry-2.jpg',
+    category: ['nature', 'flower', 'flowers', 'blossom', 'pink'],
+  },
+  {
+    id: 9,
+    img: '../../images/cherry-3.jpg',
+    category: ['nature', 'flower', 'flowers', 'blossom', 'pink'],
+  },
+  {
+    id: 10,
+    img: '../../images/currants.jpg',
+    category: ['nature', 'fruits', 'fruits', 'red', 'green'],
+  },
+  {
+    id: 11,
+    img: '../../images/gemstone.jpg',
+    category: ['nature', 'stones', 'gemstones', 'pink'],
+  },
+  {
+    id: 12,
+    img: '../../images/insect-1.jpg',
+    category: ['nature', 'animal', 'insect', 'green'],
+  },
+  {
+    id: 13,
+    img: '../../images/insect-2.jpg',
+    category: ['nature', 'animal', 'insect', 'pink'],
+  },
+  {
+    id: 14,
+    img: '../../images/insect-3.jpg',
+    category: ['nature', 'animal', 'insect', 'purple'],
+  },
+  {
+    id: 15,
+    img: '../../images/kingfisher.jpg',
+    category: ['nature', 'bird', 'animal', 'animals', 'birds', 'blue'],
+  },
+  {
+    id: 16,
+    img: '../../images/ladybug.jpg',
+    category: ['nature', 'animal', 'insect', 'ladybug', 'green'],
+  },
+  {
+    id: 17,
+    img: '../../images/levander.jpg',
+    category: ['nature', 'flower', 'flowers', 'blossom', 'purple'],
+  },
+  {
+    id: 18,
+    img: '../../images/lemon.jpg',
+    category: ['nature', 'fruits', 'lemon', 'yellow', 'green'],
+  },
+  {
+    id: 19,
+    img: '../../images/oranges.jpg',
+    category: ['nature', 'fruit', 'orange'],
+  },
+  {
+    id: 20,
+    img: '../../images/pebble.jpg',
+    category: ['nature', 'pebble', 'orange'],
+  },
+  {
+    id: 21,
+    img: '../../images/pink.jpg',
+    category: ['nature', 'flower', 'blossom', 'pink'],
+  },
+  {
+    id: 22,
+    img: '../../images/pomegranate.jpg',
+    category: ['nature', 'fruit', 'red'],
+  },
+  {
+    id: 23,
+    img: '../../images/poppies.jpg',
+    category: ['nature', 'flower', 'red', 'green'],
+  },
+  {
+    id: 24,
+    img: '../../images/spring.jpg',
+    category: ['nature', 'flower', 'pink', 'green'],
+  },
+  {
+    id: 25,
+    img: '../../images/stones-1.jpg',
+    category: ['nature', 'stones', 'blue'],
+  },
+  {
+    id: 26,
+    img: '../../images/stones-2.jpg',
+    category: ['nature', 'stones', 'blue', 'red'],
+  },
+];
